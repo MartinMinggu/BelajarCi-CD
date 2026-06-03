aaf441a5e6cdfa3787e0212dea0546f4367ceaeb
Martin Minggu <martinus.minggu@indocyber.id>
2026-06-03 14:34:40 +0700

    feat: setup MathService dengan CI pipeline

---
+++ MathService/src/MathService/MathLogic.cs
@@ -0,0 +1,5 @@
+public static class MathLogic
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Multiply(int a, int b) => a * b;
+}
---
+++ MathService/tests/MathService.Tests/MathTest.cs
@@ -0,0 +1,24 @@
+namespace MathService.Tests;
+
+public class MathTest
+{
+    [Fact]
+    public void Add_TwoNumber_ReturnsCorrectSum()
+    {
+        var result = MathLogic.Add(1, 2);
+        Assert.Equal(3, result);
+    }
+    [Fact]
+    public void Multiply_TwoNumber_ReturnsCorrectSum()
+    {
+        var result = MathLogic.Multiply(2, 2);
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void Add_NegativeNumbers_Works()
+    {
+        var result = MathLogic.Add(-3, -7);
+        Assert.Equal(-10, result);
+    }
+}
